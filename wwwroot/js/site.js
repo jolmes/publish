640b8dbc7db6bcc61008bad7d43e10ad5afe1e58
jolmesp <Jolmes.Pastora@payroll.ca>
2024-09-09 15:50:43 -0400

    First commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    if ($('.dataTable').length > 0) {
+        $('.dataTable').DataTable({ 
+            "paging": true,
+            "searching": true,
+            
+        });
+    }
+
+    $("input[type='submit']").click(function () {
+         
+    });
+});
